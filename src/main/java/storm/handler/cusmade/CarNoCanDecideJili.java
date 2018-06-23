276bf42db173cfd62ac3b2f31e7d169f30ed5edc
xuzhipeng <xuzhipeng@bitnei.cn>
2018-06-23 14:51:59 +0800

    算法优化
    - [modify] 根据李祥东确认的算法优化无CNA确认方式

--- src/main/java/storm/handler/cusmade/CarNoCanDecideJili.java
+++ src/main/java/storm/handler/cusmade/CarNoCanDecideJili.java
@@ -1,5 +1,6 @@
 package storm.handler.cusmade;
 
+import org.apache.commons.lang.StringUtils;
 import storm.system.DataKey;
 import storm.util.ObjectUtils;
 
@@ -11,135 +12,56 @@ import java.util.Map;
  * @description:
  */
 public class CarNoCanDecideJili implements ICarNoCanDecide {
-    // TODO XZP: Redis取值覆盖
-    // region 吉利判定参数
-    private static final short MIN_2201_SPEED_JILI = 0;
-    private static final short MAX_2201_SPEED_JILI = 200 * 10;
-    private static final short MIN_2613_TOTAL_VOLTAGE_JILI = 0;
-    private static final short MAX_2613_TOTAL_VOLTAGE_JILI = 1000 * 10;
-    private static final byte MIN_7615_STATE_OF_CHARGE_JILI = 0;
-    private static final byte MAX_7615_STATE_OF_CHARGE_JILI = 100;
-    private static final byte EQUALS_2501_ORIENTATION_JILI = 0;
-    private static final int MIN_2502_LONGITUDE_JILI = 73;
-    private static final int MAX_2502_LONGITUDE_JILI = 135;
-    private static final int MIN_2503_LATITUDE_JILI = 4;
-    private static final int MAX_2503_LATITUDE_JILI = 53;
-    // endregion
 
     /**
-     * 吉利报表是否有CAN判定
-     * @param data
-     * @return
+     * 吉利报表是否有CAN判定, {车速, 累计里程, 总电压, 总电流, SOC} 只要其中一个值有效, 则判定为有CAN, 否则判定为无CAN.
+     * @param data 实时数据
+     * @return 车辆是否有CAN
      */
     @Override
     public boolean hasCan(Map<String, String> data) {
 
-        // region 车速 ∈ [0, 200)
+        // region 车速
         {
             final String speedString = data.get(DataKey._2201_SPEED);
-            if (!ObjectUtils.isNullOrWhiteSpace(speedString)) {
-                try {
-                    final short speed = Short.parseShort(speedString);
-                    if (speed >= MIN_2201_SPEED_JILI && speed < MAX_2201_SPEED_JILI) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
+            if (!StringUtils.isBlank(speedString)) {
+                return true;
             }
         }
         // endregion
 
-        // TODO XZP: [待确认] 上线开始里程, 上线结束里程
-        // region 累计里程 ∈ [上线开始里程, 上线结束里程]
+        // region 累计里程
         {
             final String totalMileageString = data.get(DataKey._2202_TOTAL_MILEAGE);
-            if (!ObjectUtils.isNullOrWhiteSpace(totalMileageString)) {
-                try {
-                    final int totalMileage = Integer.parseUnsignedInt(totalMileageString);
-                    if (totalMileage >= DataKey.MIN_2202_TOTAL_MILEAGE && totalMileage <= DataKey.MAX_2202_TOTAL_MILEAGE) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
+            if (!StringUtils.isBlank(totalMileageString)) {
+                return true;
             }
         }
         // endregion
 
-        // region 总电压 ∈ (0, 1000]
+        // region 总电压
         {
             final String totalVoltageString = data.get(DataKey._2613_TOTAL_VOLTAGE);
-            if (!ObjectUtils.isNullOrWhiteSpace(totalVoltageString)) {
-                try {
-                    final short totalVoltage = Short.parseShort(totalVoltageString);
-                    if (totalVoltage > MIN_2613_TOTAL_VOLTAGE_JILI && totalVoltage <= MAX_2613_TOTAL_VOLTAGE_JILI) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
+            if (!StringUtils.isBlank(totalVoltageString)) {
+                return true;
             }
         }
         // endregion
 
-        // TODO XZP: [有时间设计算法, 没时间就用国标范围]一段时间内总电流均值, 目前算法未定.
-        // region 总电流 ∈ [总电流均值 - 100, 总电流均值 + 100]
+        // region 总电流
         {
             final String totalElectricityString = data.get(DataKey._2614_TOTAL_ELECTRICITY);
-            if (!ObjectUtils.isNullOrWhiteSpace(totalElectricityString)) {
-                try {
-                    final short totalElectricity = Short.parseShort(totalElectricityString);
-                    if (totalElectricity >= DataKey.MIN_2614_TOTAL_ELECTRICITY
-                        && totalElectricity <= DataKey.MAX_2614_TOTAL_ELECTRICITY) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
+            if (!StringUtils.isBlank(totalElectricityString)) {
+                return true;
             }
         }
         // endregion
 
-        // region SOC ∈ (0, 100]
+        // region SOC
         {
             final String stateOfChargeString = data.get(DataKey._7615_STATE_OF_CHARGE);
-            if (!ObjectUtils.isNullOrWhiteSpace(stateOfChargeString)) {
-                try {
-                    final byte stateOfCharge = Byte.parseByte(stateOfChargeString);
-                    if (stateOfCharge >= MIN_7615_STATE_OF_CHARGE_JILI
-                        && stateOfCharge <= MAX_7615_STATE_OF_CHARGE_JILI) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
-            }
-        }
-        // endregion
-
-        // region 定位状态 ∈ [0], 经度 ∈ (73, 135), 纬度 ∈ (4, 53)
-        {
-            final String orientationString = data.get(DataKey._2501_ORIENTATION);
-            final String longitudeString = data.get(DataKey._2502_LONGITUDE);
-            final String latitudeString = data.get(DataKey._2503_LATITUDE);
-            if (!ObjectUtils.isNullOrWhiteSpace(orientationString)
-                && !ObjectUtils.isNullOrWhiteSpace(longitudeString)
-                && !ObjectUtils.isNullOrWhiteSpace(latitudeString)) {
-                try {
-                    final int orientation = Integer.parseUnsignedInt(orientationString);
-                    final int longitude = Integer.parseUnsignedInt(longitudeString);
-                    final int latitude = Integer.parseUnsignedInt(latitudeString);
-                    if (orientation == EQUALS_2501_ORIENTATION_JILI
-                        && longitude >= MIN_2502_LONGITUDE_JILI
-                        && longitude <= MAX_2502_LONGITUDE_JILI
-                        && latitude >= MIN_2503_LATITUDE_JILI
-                        && latitude <= MAX_2503_LATITUDE_JILI) {
-                        return true;
-                    }
-                } catch (NumberFormatException ignored) {
-                    // nextJudge
-                }
+            if (!StringUtils.isBlank(stateOfChargeString)) {
+                return true;
             }
         }
         // endregion
