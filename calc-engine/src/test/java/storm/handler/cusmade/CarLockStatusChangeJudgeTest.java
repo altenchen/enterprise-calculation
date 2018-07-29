259839e4931edf6f42e07b0908992011c838ecb7
徐志鹏 <xuzhipeng@bitnei.cn>
2018-07-30 00:04:34 +0800

    合并小于写的动力锁止单元测试

--- calc-engine/src/test/java/storm/handler/cusmade/CarLockStatusChangeJudgeTest.java
+++ calc-engine/src/test/java/storm/handler/cusmade/CarLockStatusChangeJudgeTest.java
@@ -1,6 +1,7 @@
 package storm.handler.cusmade;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
@@ -15,6 +16,8 @@ import storm.system.NoticeType;
 import storm.system.SysDefine;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -78,6 +81,49 @@ public class CarLockStatusChangeJudgeTest {
     private void beforeEach() {
         // 每个测试之前
         VEHICLE_CACHE.delFields(TEST_VID);
+    }
+
+    //（车辆动力锁止功能状态，车辆动力锁止状态）应该只有3种组合（0,2）（1,0）（1,1）
+    @DisplayName("于心沼综合测试")
+    @Test
+    void carLockStatueChangeJudge() {
+        final Map<String, String> data = Maps.newHashMap();
+        Date date = new Date();
+
+        data.put(DataKey.VEHICLE_ID, TEST_VID);
+        data.put(DataKey.TIME, DateFormatUtils.format(date, FormatConstant.DATE_FORMAT));
+        data.put(SysDefine.MESSAGETYPE,CommandType.SUBMIT_REALTIME );
+        data.put(DataKey._4710061_JILI_LOCK_FUNCTION,"0");
+        data.put(DataKey._4710062_JILI_LOCK_STATUS,"2");
+
+        CarLockStatusChangeJudge  lock = new CarLockStatusChangeJudge();
+        Map<String, Object> notice_1 = lock.carLockStatueChangeJudge(data);
+        Assertions.assertTrue(null==notice_1,"第一帧不应该生成notice");
+
+        data.put(DataKey._4710061_JILI_LOCK_FUNCTION,"1");
+        data.put(DataKey._4710062_JILI_LOCK_STATUS,"0");
+        Map<String, Object> notice_2 = lock.carLockStatueChangeJudge(data);
+        System.out.println(notice_2.size());
+        Assertions.assertTrue(0 != notice_2.size(),"第二帧应该生成notice");
+
+        data.put(DataKey._4710061_JILI_LOCK_FUNCTION,"1");
+        data.put(DataKey._4710062_JILI_LOCK_STATUS,"1");
+        Map<String, Object> notice_3 = lock.carLockStatueChangeJudge(data);
+        Assertions.assertTrue(0 != notice_3.size(),"第三帧应该生成notice");
+
+        data.put(DataKey._4710061_JILI_LOCK_FUNCTION,"0");
+        data.put(DataKey._4710062_JILI_LOCK_STATUS,"1");
+        Map<String, Object> notice_4 = lock.carLockStatueChangeJudge(data);
+        System.out.println(notice_4.get("lockFunctionStatusChange"));
+        System.out.println(notice_4.get("lockStatusChange"));
+        Assertions.assertTrue("0".equals(notice_4.get("lockFunctionStatusChange")),"锁止功能状态应该为0，关闭");
+        Assertions.assertTrue("2".equals(notice_4.get("lockStatusChange")),"因为锁止功能状态为0，所以，虽然这帧报文的车辆锁止状态为1，已锁止，但是仍然应该为2，禁止锁止。这种情况一般不会出现");
+
+        data.put(DataKey._4710061_JILI_LOCK_FUNCTION,"3");
+        data.put(DataKey._4710062_JILI_LOCK_STATUS,"4");
+        Map<String, Object> notice_5 = lock.carLockStatueChangeJudge(data);
+        Assertions.assertTrue(null == notice_5,"状态不合法，notice应该返回null");
+
     }
 
     @DisplayName("禁用未锁")
