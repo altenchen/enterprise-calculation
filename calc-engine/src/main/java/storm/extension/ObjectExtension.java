f13190cbf9c112e7b48a1f3e349de3424d32120c
徐志鹏 <xuzhipeng@nat-ev.cn>
2018-10-12 09:20:02 +0800

    - [add] 增强对象扩展类

--- calc-engine/src/main/java/storm/extension/ObjectExtension.java
+++ calc-engine/src/main/java/storm/extension/ObjectExtension.java
@@ -3,6 +3,7 @@ package storm.extension;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import storm.util.function.SupplierE;
 
 import java.util.function.Supplier;
 
@@ -25,6 +26,14 @@ public final class ObjectExtension {
     public static <T> T defaultIfNull(
         @Nullable final T object,
         @NotNull final Supplier<? extends T> defaultValue) {
+
+        return object != null ? object : defaultValue.get();
+    }
+
+    @Contract("!null, _ -> param1")
+    public static <T, E extends Exception> T defaultIfNull(
+        @Nullable final T object,
+        @NotNull final SupplierE<? extends T, ? extends E> defaultValue) throws E {
 
         return object != null ? object : defaultValue.get();
     }
