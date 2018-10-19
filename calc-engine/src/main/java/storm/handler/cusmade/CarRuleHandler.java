49cc79d4ec7f1cb8a204f931644542ae94faec2e
沛公 <990390177@qq.com>
2018-10-19 11:30:49 +0800

    -[add] 增加了读取配置文件和redis中soc阈值的代码

--- calc-engine/src/main/java/storm/handler/cusmade/CarRuleHandler.java
+++ calc-engine/src/main/java/storm/handler/cusmade/CarRuleHandler.java
@@ -119,7 +119,7 @@ public class CarRuleHandler implements InfoNotice {
 
     private final CarNoCanJudge carNoCanJudge = new CarNoCanJudge();
 
-    private final CarLowSocJudge carLowSocJudge = new CarLowSocJudge();
+    private static final CarLowSocJudge carLowSocJudge = new CarLowSocJudge();
 
     private final CarLockStatusChangeJudge carLockStatusChangeJudge = new CarLockStatusChangeJudge();
 
